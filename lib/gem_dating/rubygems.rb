75c5881d0670601671290befd22d83dccdb382eb
Steve Jackson <steve@testdouble.com>
2023-05-09 14:56:01 -0500

    Use Gem instead of bundler for fetching
    Since we care about latest this simplifies a lot and seems more
    performant as it uses CDN caching.
    
    If we end up looking up dependency trees we'll probably need to flip
    back to the bundler index building way.

--- lib/gem_dating/rubygems.rb
+++ lib/gem_dating/rubygems.rb
@@ -1,51 +1,21 @@
-require "bundler/setup"
-require "ruby-limiter"
-
 module GemDating
   class Rubygems
-    extend Limiter::Mixin
-
-    limit_method(:limited_fetch, rate: 600)
-
     def self.fetch(...)
       new.fetch(...)
     end
 
-    attr_reader :source
-
-    def initialize(source: nil)
-      @source = source || init_source
-    end
-
     def fetch(gems)
-      gems.each { |gem| source.add_dependency_names(gem) }
       gems.map do |gem|
-        latest = source.specs.search(gem).last
-        if latest
-          limited_fetch(latest)
-        else
-          Gem::Specification.new do |s|
-            s.name = gem
-            s.version = "0.0.0.UNKNOWN"
-          end
-        end
+        Gem.latest_spec_for(gem) || unknown_version(gem)
       end
     end
 
-    def limited_fetch(gemspec)
-      source.fetchers.first.fetch_spec([gemspec.name, gemspec.version, gemspec.platform])
-    rescue
+    def unknown_version(gemname)
       Gem::Specification.new do |s|
-        s.name = gemspec.name
+        s.name = gemname
         s.version = "0.0.0.UNKNOWN"
+        s.date = "1970-01-01"
       end
-    end
-
-    def init_source
-      source = ::Bundler::Source::Rubygems.new
-      source.add_remote(::Gem.host)
-      source.remote!
-      source
     end
   end
 end
