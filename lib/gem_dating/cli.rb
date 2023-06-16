443f997fa85e818d0b1df08461c92b21a5a5c71e
Daniel Huss <63384617+Daniel-N-Huss@users.noreply.github.com>
2023-06-16 17:07:36 -0600

    simple command line class? sure!

--- lib/gem_dating/cli.rb
+++ lib/gem_dating/cli.rb
@@ -1,17 +1,23 @@
 module GemDating
   class Cli
-    SUCCESS_CODE = 0
+    SUCCESS = 0
+    GENERAL_ERROR = 1
 
     def initialize(argv)
       @argv = argv
     end
 
     def run
+      if @argv.empty?
+        $stdout << "Usage: gem_dating [GEMFILE_FILEPATH]\n"
+        return GENERAL_ERROR
+      end
+
       path = @argv.first
 
       $stdout << GemDating.from_file(path).table_print << "\n"
 
-      SUCCESS_CODE
+      SUCCESS
     end
   end
 end
