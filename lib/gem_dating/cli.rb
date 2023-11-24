b42c99f56e2c93e672b5493991f4f87b6106d8b6
Daniel Huss <63384617+Daniel-N-Huss@users.noreply.github.com>
2023-11-24 06:50:56 -0700

    Add --help options to cli
    I've opted to not go too deeply into "proper" arg parsing, or too detailed on how the cli evokes their behaviour. Will defer that to a time where we add features that change the underlying gem behaviour.

--- lib/gem_dating/cli.rb
+++ lib/gem_dating/cli.rb
@@ -3,19 +3,30 @@ module GemDating
     SUCCESS = 0
     GENERAL_ERROR = 1
 
+    HELP_TEXT =
+      <<~HELP
+        Usage: gem_dating [--help | -h] [<GEMFILE_FILEPATH>]
+      HELP
+
     def initialize(argv)
-      @argv = argv
+      args, file_path = argv.partition { |arg| (arg =~ /--\w+/) || (arg =~ /(-[a-z])/) }
+
+      @args = args
+      @file_path = file_path.first
     end
 
     def run
-      if @argv.empty?
-        $stdout << "Usage: gem_dating [GEMFILE_FILEPATH]\n"
+      if (@args & ['-h', '--help']).any?
+        $stdout << HELP_TEXT
+        return SUCCESS
+      end
+
+      unless @file_path
+        $stdout << HELP_TEXT
         return GENERAL_ERROR
       end
 
-      path = @argv.first
-
-      $stdout << GemDating.from_file(path).table_print << "\n"
+      $stdout << GemDating.from_file(@file_path).table_print << "\n"
 
       SUCCESS
     end
