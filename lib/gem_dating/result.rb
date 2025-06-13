19501753f55bed8ae4f6ef372fdb18cb917c586c
Daniel Huss <63384617+Daniel-N-Huss@users.noreply.github.com>
2025-06-13 08:54:00 -0600

    shuffle date cutoffs to result class
    Because we're not really filtering until after we get specs back from our query, I think moving this to change what Results we're returned is a more natural fit.
    
    Co-Authored-By: James O'Neill <36668255+jamesoneill997@users.noreply.github.com>

--- lib/gem_dating/result.rb
+++ lib/gem_dating/result.rb
@@ -24,5 +24,32 @@ module GemDating
     def table_print
       TablePrint::Printer.table_print(specs, [:name, :version, {date: {time_format: "%Y-%m-%d", width: 10}}]).encode("utf-8")
     end
+
+    def older_than(date)
+      specs.select! { |spec| spec.date.to_date < self.cut_off(date) }
+    end
+
+    private
+
+    def cut_off(date)
+      return unless date
+      curr_date = Date.today
+
+      number = date[0..-2].to_i
+      unit = date[-1]
+
+      case unit
+      when "y"
+        curr_date << (12 * number)
+      when "m"
+        curr_date << number
+      when "w"
+        curr_date - (number * 7)
+      when "d"
+        curr_date - number
+      else
+        raise ArgumentError, "Invalid date format: #{date}"
+      end
+    end
   end
 end
--- lib/gem_dating/rubygems.rb
+++ lib/gem_dating/rubygems.rb
@@ -6,15 +6,10 @@ module GemDating
       new.fetch(...)
     end
 
-    def fetch(gems, date)
-      specs = gems.map do |gem|
+    def fetch(gems)
+      gems.map do |gem|
         Gem.latest_spec_for(gem) || unknown_version(gem)
       end
-
-      return specs unless date
-
-      cutoff = cut_off(date)
-      specs.select { |s| s.date.to_date < cutoff }
     end
 
     def unknown_version(gemname)
@@ -22,29 +17,6 @@ module GemDating
         s.name = gemname
         s.version = "0.0.0.UNKNOWN"
         s.date = "1970-01-01"
-      end
-    end
-
-    private
-
-    def cut_off(date)
-      return unless date
-      curr_date = Date.today
-
-      number = date[0..-2].to_i
-      unit = date[-1]
-
-      case unit
-      when "y"
-        curr_date << (12 * number)
-      when "m"
-        curr_date << number
-      when "w"
-        curr_date - (number * 7)
-      when "d"
-        curr_date - number
-      else
-        raise ArgumentError, "Invalid date format: #{date}"
       end
     end
   end
